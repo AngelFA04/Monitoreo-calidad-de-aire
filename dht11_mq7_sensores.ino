92ea4fdb10fe49e2801283304b7badcab1a053fd
Haro corzo <42069572+harocorzo@users.noreply.github.com>
2018-09-17 12:42:17 -0500

    Add files via upload

---
+++ dht11_mq7_sensores.ino
@@ -0,0 +1,72 @@
+/* 
+ 
+ combinando los sensores dht11 y Mq7
+ nos basamos en el codigo dht11 e incorporamos mq7
+*/
+
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+ 
+// Definimos el pin digital donde se conecta el sensor
+#define DHTPIN 2
+// Dependiendo del tipo de sensor
+#define DHTTYPE DHT11
+
+// Definiendo el valor del sensor mq7
+int sensorValue;
+ 
+// Inicializamos el sensor DHT11
+DHT dht(DHTPIN, DHTTYPE);
+ 
+void setup() {
+  // Inicializamos comunicación serie
+  Serial.begin(9600);
+ 
+  // Comenzamos el sensor DHT
+  dht.begin();
+ 
+}
+ 
+void loop() {
+    // incorporamos el código mq7
+  sensorValue = analogRead(0);       // read analog input pin 0
+
+
+  // dht11
+    // Esperamos 5 segundos entre medidas
+  delay(5000);
+ 
+  // Leemos la humedad relativa
+  float h = dht.readHumidity();
+  // Leemos la temperatura en grados centígrados (por defecto)
+  float t = dht.readTemperature();
+  // Leemos la temperatura en grados Fahreheit
+  float f = dht.readTemperature(true);
+ 
+  // Comprobamos si ha habido algún error en la lectura
+  if (isnan(h) || isnan(t) || isnan(f)) {
+    Serial.println("Error obteniendo los datos del sensor DHT11");
+    return;
+  }
+ 
+  // Calcular el índice de calor en Fahreheit
+  float hif = dht.computeHeatIndex(f, h);
+  // Calcular el índice de calor en grados centígrados
+  float hic = dht.computeHeatIndex(t, h, false);
+ 
+  Serial.print("Humedad: ");
+  Serial.print(h);
+  Serial.print(" %\t");
+  Serial.print("Temperatura: ");
+  Serial.print(t);
+  Serial.print(" *C ");
+  Serial.print(f);
+  Serial.print(" *F\t");
+  Serial.print("indice de calor: ");
+  Serial.print(hic);
+  Serial.print(" *C ");
+  Serial.print(hif);
+  Serial.println(" *F");
+  Serial.println(sensorValue, DEC);  // prints the value read
+ 
+}
